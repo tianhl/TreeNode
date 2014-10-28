fc78998ec1d79ce27fc97e0b17f48ee2d6d063ef
tianhl <haolai.tian@gmail.com>
2014-10-28 17:29:56 +0800

    dynamic tree node

--- inc/DynamicTreeNode.h
+++ inc/DynamicTreeNode.h
@@ -2,4 +2,98 @@
 #define DYNAMIC_TREENODE_H
 
 
+#include <map>
+#include <vector>
+#include <iostream>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+
+
+
+
+class Node{
+	public:
+		virtual Node* get(const std::string& name){
+			if(!name.empty() && _map.find(name) != _map.end()) return dynamic_cast<Node*>(_map.find(name)->second);
+			else throw std::runtime_error("DynamicNode: "+name+" is not created.\n");
+		}
+		void regist(const std::string& name, Node* node){
+			if(!name.empty() && _map.find(name) == _map.end()) _map.insert(std::pair<std::string, Node*>(name, node));
+			else throw std::runtime_error(name + " is already existed.\n");
+		}
+
+	private:
+		typedef std::map<string, Node*> NodeMap;
+		NodeMap _map;
+
+};
+
+template<class O>
+class DynamicObj: public Node{
+	friend class DynamicTree;
+
+	public: O* get(const std::string& name){
+			if(m_path==name) return m_ref;
+			else throw std::runtime_error("DynamicObj "+name+" is not created.\n");
+		}
+		void regist(const std::string& name, O* obj){
+			if(name.empty()) throw std::runtime_error("regist object error: no name for object");
+			m_ref = obj;
+			m_path = name;
+		}
+	private:
+		DynamicObj(){};
+	private:
+		O* m_ref;
+		std::string m_path;
+		Node*       m_root;
+		Node*       m_parent;
+};
+
+template<class O>
+class DynamicTree: public Node{
+	public:
+		virtual Node* find(const std::string& name){
+			vector<std::string> dividedName;
+			vector<std::string>::iterator itName;
+			boost::split(dividedName, name, boost::is_any_of("/"), boost::token_compress_on);
+			Node* current = (0==name.find("/"))?m_root:this;
+			for(itName=dividedName.begin(); itName !=dividedName.end(); itName++){
+				if(0==(*itName).size()) continue;
+				current = current->get(*itName);
+				// how to throw out error? path
+			}
+			return current;
+		}
+
+		virtual Node branch(const std::string& name){
+			if(name.empty()) throw std::runtime_error("make branch error: no name for branch");
+			DynamicTree<O>* br = new DynamicTree<O>(name, this);
+			this->regist(name, br);
+			// if error delete br!
+			return br;
+		}
+
+		virtual void registObj(const std::string& name, O* o){
+			if(name.empty()) throw std::runtime_error("make branch error: no name for branch");
+			DynamicObj<O>* obj = new DynamicObj<O>();
+			obj->regist(name, o);
+		}
+
+		std::string path(){return m_path;}
+		Node*       root(){return m_root;}
+	private:
+		DynamicTree(std::string name, DynamicTree<O> parent){
+			m_parent = parent;
+			m_root   = parent->root();
+			m_path   = m_parent->path()+(m_parent->path()=="/"?name:"/"+name);
+		}
+
+	private:
+		std::string      m_path;
+		DynamicTree<O>*  m_root;
+		DynamicTree<O>*  m_parent;
+};
+
+
 #endif
