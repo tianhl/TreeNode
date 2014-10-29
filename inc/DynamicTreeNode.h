dbe7b0dcf47fc6335577f1fa9bffce83647f7b92
tianhl <haolai.tian@gmail.com>
2014-10-29 11:02:21 +0800

    first version

--- inc/DynamicTreeNode.h
+++ inc/DynamicTreeNode.h
@@ -13,7 +13,9 @@
 
 class Node{
 	public:
-		virtual Node* get(const std::string& name){
+		//virtual std::string path() = 0;
+		//virtual Node* get(const std::string& name){
+		virtual Node* find(const std::string& name){
 			if(!name.empty() && _map.find(name) != _map.end()) return dynamic_cast<Node*>(_map.find(name)->second);
 			else throw std::runtime_error("DynamicNode: "+name+" is not created.\n");
 		}
@@ -23,50 +25,60 @@ class Node{
 		}
 
 	private:
-		typedef std::map<string, Node*> NodeMap;
+		typedef std::map<std::string, Node*> NodeMap;
 		NodeMap _map;
 
 };
 
+
 template<class O>
 class DynamicObj: public Node{
-	friend class DynamicTree;
+	//friend class DynamicTree<O>;
 
-	public: O* get(const std::string& name){
-			if(m_path==name) return m_ref;
-			else throw std::runtime_error("DynamicObj "+name+" is not created.\n");
+	public: O* get(){ 
+			if(m_ref) return m_ref; 
+			else throw std::runtime_error("DynamicObj is not registed.\n");
 		}
 		void regist(const std::string& name, O* obj){
 			if(name.empty()) throw std::runtime_error("regist object error: no name for object");
-			m_ref = obj;
-			m_path = name;
+			m_ref = obj; m_name = name;
 		}
-	private:
-		DynamicObj(){};
+		virtual Node* find(std::string name){
+			if(name==m_name) return this;
+			else throw std::runtime_error("DynamicObj "+name+" is not created.\n");
+		}
+		//virtual std::string path()
+		DynamicObj(){}
+	//private:
+	//	DynamicObj(){};
 	private:
 		O* m_ref;
-		std::string m_path;
-		Node*       m_root;
-		Node*       m_parent;
+		std::string m_name;
 };
 
 template<class O>
 class DynamicTree: public Node{
 	public:
+		DynamicTree(){
+			m_parent = 0;
+			m_root   = this;
+			m_path   = "/";
+		}
+
 		virtual Node* find(const std::string& name){
 			vector<std::string> dividedName;
 			vector<std::string>::iterator itName;
 			boost::split(dividedName, name, boost::is_any_of("/"), boost::token_compress_on);
 			Node* current = (0==name.find("/"))?m_root:this;
-			for(itName=dividedName.begin(); itName !=dividedName.end(); itName++){
+			for(itName=dividedName.begin(); itName!=dividedName.end(); itName++){
 				if(0==(*itName).size()) continue;
-				current = current->get(*itName);
+				current = current->Node::find(*itName);
 				// how to throw out error? path
 			}
 			return current;
 		}
 
-		virtual Node branch(const std::string& name){
+		virtual Node* branch(const std::string& name){
 			if(name.empty()) throw std::runtime_error("make branch error: no name for branch");
 			DynamicTree<O>* br = new DynamicTree<O>(name, this);
 			this->regist(name, br);
@@ -78,12 +90,14 @@ class DynamicTree: public Node{
 			if(name.empty()) throw std::runtime_error("make branch error: no name for branch");
 			DynamicObj<O>* obj = new DynamicObj<O>();
 			obj->regist(name, o);
+			this->regist(name, obj);
+			std::cout << "registObj Obj " << obj << " data " << o << std::endl;
 		}
 
-		std::string path(){return m_path;}
-		Node*       root(){return m_root;}
+		std::string     path(){return m_path;}
+		DynamicTree<O>* root(){return m_root;}
 	private:
-		DynamicTree(std::string name, DynamicTree<O> parent){
+		DynamicTree(std::string name, DynamicTree<O>* parent){
 			m_parent = parent;
 			m_root   = parent->root();
 			m_path   = m_parent->path()+(m_parent->path()=="/"?name:"/"+name);
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,29 @@
 using namespace std;
 
 #include "DynamicTreeNode.h"
+#include "Pulse.h"
+#include "Event.h"
+
 int main(int argc, char *argv[]) {
+
+	typedef DynamicTree<DataObject> DataStore;
+	typedef DynamicObj<DataObject> Data;
+
+	DataStore* datastore = new DataStore();
+	std::cout << "root datastore: " << datastore->path() << std::endl;
+	std::cout << dynamic_cast<DataStore*>(datastore->find("/")) << std::endl;
+	std::cout << "new branch " << datastore->branch("pulse") << std::endl;
+	std::cout << dynamic_cast<DataStore*>(datastore->find("/pulse"))->path() << std::endl;
+	dynamic_cast<DataStore*>(datastore->find("/pulse"))->branch("event");
+	std::cout << dynamic_cast<DataStore*>(datastore->find("pulse/event"))->path() << std::endl;
+
+	dynamic_cast<DataStore*>(datastore->find("/pulse"))->registObj("pulseHdr", new Pulse());
+	std::cout << "======================" << std::endl;
+	Data* data = dynamic_cast<Data*>(datastore->find("/pulse/pulseHdr"));
+	std::cout << "obj: " << data << " data " << data->get() << " type "<<  typeid(data->get()).name() << std::endl;
+	// error here: 
+	dynamic_cast<Pulse*>(data->get());
+
+
 	return 1;
 }
